758917c8e03c11bb2f43815a3c2a954b2272272d
tommyal47 <ahmedali.al047@gmail.com>
2024-09-14 03:13:19 +0300

    update configration

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -1,26 +1,41 @@
 <template>
-    <v-sheet class="mx-auto" width="300">
-      <v-form @submit.prevent>
-        <v-text-field
-          v-model="firstName"
-          :rules="rules"
-          label="First name"
-        ></v-text-field>
-        <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-      </v-form>
-    </v-sheet>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        firstName: '',
-        rules: [
-          value => {
-            if (value) return true
-  
-            return 'You must enter a first name.'
-          },
-        ],
-      }),
-    }
-  </script>
+  <v-sheet class="mx-auto" width="300">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        :rules="firstNameRules"
+        label="First name"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="lastName"
+        :rules="lastNameRules"
+        label="Last name"
+      ></v-text-field>
+
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+<script>
+  export default {
+    data: () => ({
+      firstName: '',
+      firstNameRules: [
+        value => {
+          if (value?.length >= 3) return true
+
+          return 'First name must be at least 3 characters.'
+        },
+      ],
+      lastName: '123',
+      lastNameRules: [
+        value => {
+          if (/[^0-9]/.test(value)) return true
+
+          return 'Last name can not contain digits.'
+        },
+      ],
+    }),
+  }
+</script>
