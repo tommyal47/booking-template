11654c5aa9e7967ad0432ed200e83fa4e022fc15
tommyal47 <ahmedali.al047@gmail.com>
2024-09-16 17:20:26 +0300

    show user

--- src/components/IndexUsers.vue
+++ src/components/IndexUsers.vue
@@ -2,19 +2,23 @@
     <v-data-table :headers="headers" :items="userStore.users" density="compact" item-key="name">
         <template v-slot:item.actions>
             <div class="d-flex justify-space-around flex-wrap pa-2 ml-30">
-                <v-btn class="ma-2" color="green-lighten-1" size="33px" icon="mdi-eye"></v-btn>
+                <v-btn class="ma-2" @click="showUser" color="green-lighten-1" size="33px" icon="mdi-eye"></v-btn>
                 <v-btn class="ma-2" color="green-lighten-1" size="33px" icon="mdi-pencil"></v-btn>
                 <v-btn class="ma-2" color="orange-lighten-1" size="33px" icon="mdi-delete"></v-btn>
             </div>
         </template>
     </v-data-table>
     <v-btn @click="router.push('/add_user')">back</v-btn>
+    <div v-if="openShowDialog">
+        <ShowUser @handleCloseDialog="handleCloseDialog" />
+    </div>
 </template>
 
 
 <script setup>
 
 import { useUserStore } from '@/stores/storeUser';
+import ShowUser from './ShowUser.vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 const router = useRouter()
@@ -28,7 +32,15 @@ const headers = ref([
     { title: 'actions', align: 'center', key: 'actions' }
 ])
 
+const openShowDialog = ref(false);
 
+const showUser = () => {
+    openShowDialog.value = true;
+}
 console.log(userStore.users);
+
+const handleCloseDialog = () => {
+    openShowDialog.value = false;
+}
 
 </script>
---
+++ src/components/ShowUser.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="text-center pa-4">
+        <v-dialog v-model="dialog" persistent width="auto">
+            <v-card max-width="400" prepend-icon="mdi-update"
+                text="Your application will relaunch automatically after the update is complete."
+                title="Update in progress">
+                <template v-slot:actions>
+                    <v-btn class="ms-auto" text="Ok" @click="$emit('handleCloseDialog')"></v-btn>
+                </template>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+
+const dialog = ref(true);
+const emit = defineEmits(['handleCloseDialog'])
+</script>
