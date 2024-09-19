b7bc17774cca0e2eacf78f980f29723d37f499ae
tommyal47 <ahmedali.al047@gmail.com>
2024-09-19 20:41:26 +0300

    add items to side bar

--- src/layouts/components/SideBar.vue
+++ src/layouts/components/SideBar.vue
@@ -2,13 +2,16 @@
     <v-card>
         <v-layout>
             <v-navigation-drawer expand-on-hover rail>
-                <v-list>
+                <!-- <v-list>
                     <v-list-item></v-list-item>
-                </v-list>
+                </v-list> -->
                 <v-divider></v-divider>
-                <v-list density="compact" nav>
+                <v-list density="compact" nav class="mt-16">
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-home" title="Home" value="Home"
+                        @click="goToHome">
+                    </v-list-item>
                     <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-multiple" title="Users"
-                        value="Users" :active @click="goToUseres">
+                        value="Users" @click="goToUseres">
                     </v-list-item>
                 </v-list>
             </v-navigation-drawer>
@@ -18,13 +21,20 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
-import { onMounted, ref } from 'vue';
+// import { onMounted, ref } from 'vue';
 
-const active = ref(true);
+// const active = ref(true);
 const router = useRouter();
+const goToHome = () => {
+    // Navigate to the Users page
+    // active.value = true;
+    // console.log(router.currentRoute.value.path);
+
+    router.push('/');
+}
 const goToUseres = () => {
     // Navigate to the Users page
-    active.value = true;
+    // active.value = true;
     // console.log(router.currentRoute.value.path);
 
     router.push('/users');
