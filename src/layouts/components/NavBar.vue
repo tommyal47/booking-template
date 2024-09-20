20c4ba4ed31f70f0213025740143206ac78cc10e
tommyal47 <ahmedali.al047@gmail.com>
2024-09-20 16:17:29 +0300

    add language icon

--- src/layouts/components/NavBar.vue
+++ src/layouts/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
     <v-layout class="rounded rounded-md">
         <v-app-bar color="green-darken-1" title="Application bar">
+            <v-btn style="margin-right: -400px;" @click="changeLanguage" icon="mdi-translate" rounded="shaped"></v-btn>
             <v-btn class="log-btn" @click="handleLogout">logout</v-btn>
         </v-app-bar>
     </v-layout>
@@ -10,6 +11,11 @@
 const handleLogout = () => {
     localStorage.clear();
     window.location.href = '/login';
+}
+
+const changeLanguage = () => {
+    console.log('change language');
+    
 }
 
 </script>
