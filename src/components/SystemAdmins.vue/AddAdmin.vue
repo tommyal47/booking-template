fc650a578840aca142c8e06d4ee10960e5e8243c
tommyal47 <ahmedali.al047@gmail.com>
2024-10-03 19:08:35 +0300

    update stepper

--- src/components/SystemAdmins.vue/AddAdmin.vue
+++ src/components/SystemAdmins.vue/AddAdmin.vue
@@ -4,17 +4,26 @@
     <v-dialog v-model="dialog" persistent>
       <v-card class="mx-auto" width="500" prepend-icon="" title="Add Admin">
         <v-sheet class="mx-auto" width="500">
-          <v-stepper :items="['Step 1', 'Step 2', 'Step 3']">
+          <v-stepper :items="items">
             <template v-slot:item.1>
-              <v-card title="Step One" flat>...</v-card>
+              <v-card>
+                <v-text-field clearable label="full name" variant="solo"></v-text-field>
+                <v-text-field clearable label="email" variant="solo"></v-text-field>
+                <v-text-field clearable label="phone" variant="solo"></v-text-field>
+              </v-card>
             </template>
 
             <template v-slot:item.2>
-              <v-card title="Step Two" flat>...</v-card>
+              <v-card>
+                <v-text-field clearable label="role" variant="solo"></v-text-field>
+              </v-card>
             </template>
 
             <template v-slot:item.3>
-              <v-card title="Step Three" @click="$emit('handleCloseDialog')" flat>...</v-card>
+              <v-card @click="$emit('handleCloseDialog')">
+                <v-text-field clearable label="password" variant="solo"></v-text-field>
+                <v-text-field clearable label="confirm password" variant="solo"></v-text-field>
+              </v-card>
             </template>
           </v-stepper>
         </v-sheet>
@@ -28,4 +37,6 @@ import { ref, defineEmits } from 'vue';
 
 const dialog = ref(true);
 defineEmits('[handleCloseDialog]')
+
+const items = ref(['Admin info', 'Role', 'Password'])
 </script>
