7422738e6d94f47396b5bb3fd30d61ff406d0f50
tommyal47 <ahmedali.al047@gmail.com>
2024-10-13 16:11:54 +0300

    add permissions tab

--- src/layouts/components/SideBar.vue
+++ src/layouts/components/SideBar.vue
@@ -4,17 +4,23 @@
             <v-navigation-drawer expand-on-hover rail>
                 <v-divider></v-divider>
                 <v-list density="compact" nav class="mt-16">
-                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-home" :title="$t('Home')" value="Home"
-                        @click="goToHome" :active="isActive('/')">
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-home" :title="$t('Home')"
+                        value="Home" @click="goToHome" :active="isActive('/')">
                     </v-list-item>
-                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-multiple" :title="$t('Users')"
-                        value="Users" @click="goToUseres" :active="isActive('/users')">
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-multiple"
+                        :title="$t('Users')" value="Users" @click="goToUseres" :active="isActive('/users')">
                     </v-list-item>
-                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-key" :title="$t('RolesLable')"
-                        value="Roles" @click="$router.push('/roles')" :active="isActive('/roles')">
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-key"
+                        :title="$t('RolesLable')" value="Roles" @click="$router.push('/roles')"
+                        :active="isActive('/roles')">
                     </v-list-item>
-                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-supervisor" :title="$t('Admins')"
-                        value="Roles" @click="$router.push('/admins')" :active="isActive('/admins')">
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-supervisor"
+                        :title="$t('Admins')" value="Roles" @click="$router.push('/admins')"
+                        :active="isActive('/admins')">
+                    </v-list-item>
+                    <v-list-item color="primary" rounded="shaped" prepend-icon="mdi-account-key"
+                        :title="$t('Permissions')" value="Roles" @click="$router.push('/permissions')"
+                        :active="isActive('/permissions')">
                     </v-list-item>
                 </v-list>
             </v-navigation-drawer>
