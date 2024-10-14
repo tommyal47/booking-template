bf5b9a363f884446ef8111354bf0642705b83969
tommyal47 <ahmedali.al047@gmail.com>
2024-10-14 12:31:12 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,10 @@ import MainLayout from './layouts/MainLayout.vue';
 
 const loggedIn = ref(localStorage.getItem('auth'));
 
-// const handleLogout = () => {
-//   localStorage.clear();
-//   window.location.href = '/login';
-// }
-
 </script>
 
 <template>
   <MainLayout v-if="loggedIn" />
-  <!-- <v-btn class="log-btn" v-if="loggedIn" @click="handleLogout">logout</v-btn> -->
   <RouterView />
 </template>
 
@@ -24,3 +18,19 @@ const loggedIn = ref(localStorage.getItem('auth'));
 
 }
 </style>
+
+
+
+<!-- <template>
+  <v-app>
+    <v-app-bar color="green-darken-1" :title="$t('ApplicationName')" app>
+      <NavBar />
+    </v-app-bar>
+    <v-navigation-drawer expand-on-hover rail app>
+      <SideBar />
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template> -->
