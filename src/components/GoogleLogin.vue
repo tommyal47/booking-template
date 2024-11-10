28fb6920bea7b36aa91ccddcc07e63dd098a3d17
tommyal47 <ahmedali.al047@gmail.com>
2024-11-10 16:51:34 +0200

    getting user info

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -10,11 +10,16 @@ import {
     type CredentialResponse,
 } from "vue3-google-signin";
 
+// let token = null;
 // handle success event
 const handleLoginSuccess = (response: CredentialResponse) => {
     const { credential } = response;
+    // token = credential
+
     saveAuth()
     localStorage.setItem('role', JSON.stringify(role))
+    localStorage.setItem('token', credential)
+    localStorage.setItem('token', credential)
     window.location.href = '/'
     console.log("Access Token", credential);
 };
@@ -23,6 +28,16 @@ const handleLoginSuccess = (response: CredentialResponse) => {
 const handleLoginError = () => {
     console.error("Login failed");
 };
+
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 </script>
 
 <template>
